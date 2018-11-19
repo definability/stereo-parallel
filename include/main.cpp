3d282c31136315fa80ac78ee33f74313e9c36b60
char-lie <5962274+char-lie@users.noreply.github.com>
2018-11-19 21:04:00 +0200

    Add main file
    Boost Program Options usage
    ===========================
    
    Logical way to start using Boost Program Options
    is the tutorial
    https://www.boost.org/doc/libs/1_68_0/doc/html/program_options/tutorial.html
    
    It looks messy, but all becomes clear with full code example
    https://www.boost.org/doc/libs/1_57_0/libs/program_options/example/first.cpp
    
    I didn't find any default tools
    to check whether input parameter is an existent file,
    so I've made my own
    (also checked whether it's a correct plain PGM image file).
    In order to check whether all is good we simply use `operator!`,
    which is a synonym to `fail()` member function
    http://www.cplusplus.com/reference/ios/ios/fail/
    
    Exceptions handling
    ===================
    
    The code example has an ugly `try-catch` block within.
    I know a modern approach called `Function-try-block`.
    You should not create one more indentation level,
    you just write a `try` word after the function signature
    and use `catch` after the function block
    https://en.cppreference.com/w/cpp/language/function-try-block
    
    I didn't know whether I have to use `\n` or `std::endl`
    in error output (`std::cerr` may be a regular file).
    Example contains `\n`, but I don't like to use it in modern C++.
    My concern was caused by the fact
    that Python has a similar thing called `os.linesep`,
    and documentation says that I shouldn't use it as line ending in files,
    and I should use `\n` instead
    https://docs.python.org/3/library/os.html#os.linesep
    Though, it seems like C++ has no such limitation
    and I can use `std::endl` freely for line endings in files
    https://stackoverflow.com/a/213977/1305036

---
+++ include/main.cpp
@@ -0,0 +1,100 @@
+#include <boost/program_options.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+#include <pgm_io.hpp>
+#include <image.hpp>
+
+struct Image read_image(std::string image_path);
+
+int main(int argc, char* argv[]) try
+{
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "Help message")
+        ("left-image,l",
+         boost::program_options::value<std::string>(),
+         "Left image")
+        ("right-image,r",
+         boost::program_options::value<std::string>(),
+         "Right image");
+
+    boost::program_options::variables_map vm;
+    boost::program_options::store(
+        boost::program_options::parse_command_line(argc, argv, desc),
+        vm
+    );
+    boost::program_options::notify(vm);    
+
+    if (vm.count("left-image") && vm.count("right-image"))
+    {
+        struct Image left_image;
+        struct Image right_image;
+        try {
+            left_image = read_image(vm["left-image"].as<std::string>());
+            right_image = read_image(vm["right-image"].as<std::string>());
+        }
+        catch (std::invalid_argument& e)
+        {
+            std::cerr << "Invalid argument: " << e.what() << std::endl;
+        }
+        catch (std::logic_error& e)
+        {
+            std::cerr << "Logic error: " << e.what() << std::endl;
+        }
+    }
+    else if (vm.count("left-image") || vm.count("right-image"))
+    {
+        std::cerr
+            << "You should specify both left and right image."
+            << std::endl;
+    }
+    else
+    {
+        std::cout << desc;
+        return 1;
+    }
+    return 0;
+}
+catch(std::exception& e) {
+    std::cerr << "Unexpected exception: " << e.what() << std::endl;
+    return 1;
+}
+catch(...) {
+    std::cerr << "Exception of unknown type!" << std::endl;
+    return 1;
+}
+
+struct Image read_image(std::string image_path)
+{
+    std::ifstream image_file(image_path);
+    if (!image_file)
+    {
+        throw std::invalid_argument(
+            "Unable to open file `" + image_path + "`."
+        );
+    }
+
+    PGM_IO pgm_io;
+    image_file >> pgm_io;
+    if (!image_file)
+    {
+        throw std::invalid_argument(
+            "File `" + image_path +
+            "` is not a correct plain PGM image."
+        );
+    }
+    if (!pgm_io.get_image())
+    {
+        throw std::logic_error(
+            "Image `" + image_path + "` is valid, "
+            "but it wasn't read for some reason. "
+            "Please, report the issue to developers."
+        );
+    }
+    return *pgm_io.get_image();
+}
