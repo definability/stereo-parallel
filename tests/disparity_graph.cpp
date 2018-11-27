7f30651b7ac01ab19ec4d31e00d7afbe07733e65
char-lie <5962274+char-lie@users.noreply.github.com>
2018-11-27 22:27:53 +0200

    Add continuity check tests

--- tests/disparity_graph.cpp
+++ tests/disparity_graph.cpp
@@ -118,6 +118,41 @@ BOOST_AUTO_TEST_CASE(check_edges_existence)
     BOOST_CHECK(!edge_exists(disparity_graph, {{{0, 1}, 0}, {{0, 1}, 2}}));
 }
 
+BOOST_AUTO_TEST_CASE(check_continuity_constraint)
+{
+    PGM_IO pgm_io;
+    std::istringstream left_image_content{R"left_image(
+    P2
+    3 2
+    2
+    0 1 2
+    2 0 1
+    )left_image"};
+    std::istringstream right_image_content{R"right_image(
+    P2
+    3 2
+    2
+    1 0 2
+    2 0 1
+    )right_image"};
+
+    left_image_content >> pgm_io;
+    BOOST_REQUIRE(pgm_io.get_image());
+    struct Image left_image{*pgm_io.get_image()};
+
+    right_image_content >> pgm_io;
+    BOOST_REQUIRE(pgm_io.get_image());
+    struct Image right_image{*pgm_io.get_image()};
+
+    struct DisparityGraph disparity_graph{left_image, right_image, 2};
+
+    BOOST_CHECK(!edge_exists(disparity_graph, {{{0, 0}, 2}, {{0, 1}, 0}}));
+    BOOST_CHECK(!edge_exists(disparity_graph, {{{0, 1}, 0}, {{0, 0}, 2}}));
+
+    BOOST_CHECK(!edge_exists(disparity_graph, {{{1, 0}, 2}, {{1, 1}, 0}}));
+    BOOST_CHECK(!edge_exists(disparity_graph, {{{1, 1}, 0}, {{1, 0}, 2}}));
+}
+
 BOOST_AUTO_TEST_CASE(
     check_initial_edges_penalties,
     *boost::unit_test::tolerance(0.5)
