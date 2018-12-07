27e7094e82c02b0c341988de6e8f24dcd7b3b27f
char-lie <5962274+char-lie@users.noreply.github.com>
2018-12-07 20:09:33 +0200

    Move documentation with formulas to ConstraintGraph description

--- include/constraint_graph.hpp
+++ include/constraint_graph.hpp
@@ -45,11 +45,42 @@ using BOOL_ARRAY = std::vector<BOOL>;
 
 /**
  * \brief Structure to represent a graph with constraints
- * on choice of disparities for pixels (CSP problem).
+ * on choice of disparities for pixels (constraint satisfaction problem, CSP).
  *
  * After optimization performed on DisparityGraph,
  * it's needed to choose one labeling of many others
  * that satisfy constraints of the problem.
+ *
+ * Assuming the \f$\mathbb{I}\f$ to be an indicator function,
+ * we have the following formulas to set initial constraints for nodes
+ *
+ * \f[
+ *  b_i\left( d \right)
+ *  = \mathbb{I}\left(
+ *      q_i\left( d; \varphi \right)
+ *      - \min\limits_{\delta \in D}{q_i\left( \delta; \varphi \right)}
+ *      \le \varepsilon
+ *  \right),
+ *  \quad i \in I,
+ *  \quad d \in D
+ * \f]
+ *
+ * and edges
+ *
+ * \f[
+ *  a_{ij}\left( d, d' \right)
+ *  = \mathbb{I}\left(
+ *      g_{ij}\left( d, d'; \varphi \right)
+ *      - \min\limits_{\left\langle \delta, \delta' \right\rangle \in D^2}{
+ *          g_{ij}\left( \delta, \delta'; \varphi \right)}
+ *      \le \varepsilon
+ *  \right),
+ *  \quad i \in I,
+ *  \quad j \in N_i,
+ *  \quad \left\langle d, d' \right\rangle \in D^2.
+ * \f]
+ *
+ * There are cases when a constraint satisfaction problem
  */
 struct ConstraintGraph
 {
@@ -60,13 +91,6 @@ struct ConstraintGraph
      * `true` means that the node can be chosen under applied constraints.
      */
     BOOL_ARRAY nodes_availability;
-    /**
-     * \brief Array that contains markers for availability of edges.
-     *
-     * `false` means that the edge cannot be chosen.
-     * `true` means that the edge can be chosen under applied constraints.
-     */
-    BOOL_ARRAY edges_availability;
 };
 
 /**
@@ -81,55 +105,6 @@ struct ConstraintGraph
  * The same procedure is performed for each Edge:
  * if an Edge is worse than the best one not more than for `threshold`,
  * then it's marked as available.
- *
- * Denoting vertex penalty
- *
- * \f[
- *  q_i\left( d; \Phi \right)
- *  = \left\|
- *        R\left( i^x, i^y \right) - L\left( i^x + d, i^y \right)
- *    \right\|^p
- *    + \sum\limits_{j \in \mathcal{N}_i}
- *        \varphi_{i j}\left( d \right)
- * \f]
- *
- * and edge penalty
- *
- * \f[
- *  g_{ij}\left( d, d'; \Phi \right)
- *  = \left\| d - d' \right\|^p
- *    - \varphi_{i j}\left( d \right)
- *    - \varphi_{j i}\left( d' \right),
- * \f]
- *
- * assuming the \f$\mathbb{I}\f$ to be an indicator function,
- * we have the following formulas to set initial constraints for nodes
- *
- * \f[
- *  b_i\left( d \right)
- *  = \mathbb{I}\left(
- *      q_i\left( d; \Phi \right)
- *      - \min\limits_{\delta \in D}{q_i\left( \delta; \Phi \right)}
- *      \le \varepsilon
- *  \right),
- *  \quad i \in I,
- *  \quad d \in D
- * \f]
- *
- * and edges
- *
- * \f[
- *  a_{ij}\left( d, d' \right)
- *  = \mathbb{I}\left(
- *      g_{ij}\left( d, d'; \Phi \right)
- *      - \min\limits_{\left\langle \delta, \delta' \right\rangle \in D^2}{
- *          g_{ij}\left( \delta, \delta'; \Phi \right)}
- *      \le \varepsilon
- *  \right),
- *  \quad i \in I,
- *  \quad j \in N_i,
- *  \quad \left\langle d, d' \right\rangle \in D^2.
- * \f]
  */
 struct ConstraintGraph disparity2constraint(
     const struct DisparityGraph& disparity_graph,
