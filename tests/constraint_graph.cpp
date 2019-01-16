54b4484e45df7fc6248f1b4af37a8bac73f5f650
char-lie <5962274+char-lie@users.noreply.github.com>
2019-01-16 20:24:19 +0200

    Use new constructor of constraint graph in tests

--- tests/constraint_graph.cpp
+++ tests/constraint_graph.cpp
@@ -26,6 +26,7 @@
 #include <constraint_graph.hpp>
 #include <disparity_graph.hpp>
 #include <image.hpp>
+#include <lowest_penalties.hpp>
 #include <pgm_io.hpp>
 
 BOOST_AUTO_TEST_SUITE(ConstraintGraphTest)
@@ -46,14 +47,14 @@ BOOST_AUTO_TEST_CASE(check_nodes_indexing)
     struct Image image{*pgm_io.get_image()};
 
     struct DisparityGraph disparity_graph{image, image, 3, 1, 1};
-    struct ConstraintGraph constraint_graph{disparity2constraint(disparity_graph, 1)};
-    BOOST_REQUIRE_EQUAL(constraint_graph.disparity_graph, &disparity_graph);
+    struct LowestPenalties lowest_penalties{disparity_graph};
+    struct ConstraintGraph constraint_graph{disparity_graph, lowest_penalties, 1};
 
-    BOOST_CHECK_EQUAL(node_index(*(constraint_graph.disparity_graph), {{0, 0}, 0}), 0);
-    BOOST_CHECK_EQUAL(node_index(*(constraint_graph.disparity_graph), {{0, 0}, 2}), 2);
-    BOOST_CHECK_EQUAL(node_index(*(constraint_graph.disparity_graph), {{0, 1}, 0}), 3);
-    BOOST_CHECK_EQUAL(node_index(*(constraint_graph.disparity_graph), {{0, 1}, 2}), 5);
-    BOOST_CHECK_EQUAL(node_index(*(constraint_graph.disparity_graph), {{1, 0}, 0}), 6);
+    BOOST_CHECK_EQUAL(node_index(constraint_graph.disparity_graph, {{0, 0}, 0}), 0);
+    BOOST_CHECK_EQUAL(node_index(constraint_graph.disparity_graph, {{0, 0}, 2}), 2);
+    BOOST_CHECK_EQUAL(node_index(constraint_graph.disparity_graph, {{0, 1}, 0}), 3);
+    BOOST_CHECK_EQUAL(node_index(constraint_graph.disparity_graph, {{0, 1}, 2}), 5);
+    BOOST_CHECK_EQUAL(node_index(constraint_graph.disparity_graph, {{1, 0}, 0}), 6);
 }
 
 BOOST_AUTO_TEST_CASE(check_black_images)
@@ -72,8 +73,8 @@ BOOST_AUTO_TEST_CASE(check_black_images)
     struct Image image{*pgm_io.get_image()};
 
     struct DisparityGraph disparity_graph{image, image, 3, 1, 1};
-    struct ConstraintGraph constraint_graph{disparity2constraint(disparity_graph, 1)};
-    BOOST_REQUIRE_EQUAL(constraint_graph.disparity_graph, &disparity_graph);
+    struct LowestPenalties lowest_penalties{disparity_graph};
+    struct ConstraintGraph constraint_graph{disparity_graph, lowest_penalties, 1};
     BOOST_CHECK_CLOSE(constraint_graph.threshold, 1, 1);
 
     struct Node node{{0, 0}, 0};
@@ -121,8 +122,8 @@ BOOST_AUTO_TEST_CASE(check_equal_images)
     struct Image image{*pgm_io.get_image()};
 
     struct DisparityGraph disparity_graph{image, image, 3, 1, 1};
-    struct ConstraintGraph constraint_graph{disparity2constraint(disparity_graph, 128 * 128)};
-    BOOST_REQUIRE_EQUAL(constraint_graph.disparity_graph, &disparity_graph);
+    struct LowestPenalties lowest_penalties{disparity_graph};
+    struct ConstraintGraph constraint_graph{disparity_graph, lowest_penalties, 128 * 128};
     BOOST_CHECK_CLOSE(constraint_graph.threshold, 128 * 128, 1);
 
     BOOST_CHECK_CLOSE(node_penalty(disparity_graph, {{0, 0}, 0}), 0, 1);
@@ -171,8 +172,8 @@ BOOST_AUTO_TEST_CASE(check_disparity_loop)
     struct Image right_image{*pgm_io.get_image()};
 
     struct DisparityGraph disparity_graph{left_image, right_image, 2, 1, 1};
-    struct ConstraintGraph constraint_graph{disparity2constraint(disparity_graph, 15)};
-    BOOST_REQUIRE_EQUAL(constraint_graph.disparity_graph, &disparity_graph);
+    struct LowestPenalties lowest_penalties{disparity_graph};
+    struct ConstraintGraph constraint_graph{disparity_graph, lowest_penalties, 15};
     BOOST_CHECK_CLOSE(constraint_graph.threshold, 15, 1);
 
     BOOST_CHECK_CLOSE(node_penalty(disparity_graph, {{0, 0}, 0}), 4, 1);
@@ -220,8 +221,8 @@ BOOST_AUTO_TEST_CASE(check_minimal_node_value_calculation)
     struct Image right_image{*pgm_io.get_image()};
 
     struct DisparityGraph disparity_graph{left_image, right_image, 3, 1, 1};
-    struct ConstraintGraph constraint_graph{disparity2constraint(disparity_graph, 0.5)};
-    BOOST_REQUIRE_EQUAL(constraint_graph.disparity_graph, &disparity_graph);
+    struct LowestPenalties lowest_penalties{disparity_graph};
+    struct ConstraintGraph constraint_graph{disparity_graph, lowest_penalties, 0.5};
     BOOST_CHECK_CLOSE(constraint_graph.threshold, 0.5, 1);
 
     BOOST_CHECK_CLOSE(node_penalty(disparity_graph, {{2, 0}, 0}), 1, 1);
