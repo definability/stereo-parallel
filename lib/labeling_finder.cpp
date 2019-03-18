5469670b48bbdf9a3b93a8c2f52957a3dedee7d2
char-lie <5962274+char-lie@users.noreply.github.com>
2019-03-18 21:37:41 +0200

    Fix includes order

--- lib/labeling_finder.cpp
+++ lib/labeling_finder.cpp
@@ -21,9 +21,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#include <labeling_finder.hpp>
 #include <constraint_graph.hpp>
 #include <disparity_graph.hpp>
+#include <labeling_finder.hpp>
 #include <lowest_penalties.hpp>
 
 #include <algorithm>
