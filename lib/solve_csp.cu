21e3837444ddccf9c43e0007712a228bf835120e
char-lie <5962274+char-lie@users.noreply.github.com>
2020-03-21 23:03:46 +0200

    Initiate CUDA device code
    Start with function, populating the structures.
    
    Use `#if defined(__CUDA_ARCH__)` in the function, or the error will
    occur
    
    ```
    .../lib/solve_csp.cu(47): error: no operator "=" matches these operands
                operand types are: sp::types::ULONG_ARRAY = unsigned int *
    
    .../lib/solve_csp.cu(52): error: no operator "=" matches these operands
                operand types are: sp::types::ULONG_ARRAY = unsigned int *
    
    .../lib/solve_csp.cu(59): error: no operator "=" matches these operands
                operand types are: sp::types::FLOAT_ARRAY = float *
    
    .../lib/solve_csp.cu(63): error: no operator "=" matches these operands
                operand types are: sp::types::FLOAT_ARRAY = float *
    
    .../lib/solve_csp.cu(64): error: no operator "=" matches these operands
                operand types are: sp::types::FLOAT_ARRAY = float *
    
    .../lib/solve_csp.cu(67): error: no operator "=" matches these operands
                operand types are: sp::types::BOOL_ARRAY = int *
    
    6 errors detected in the compilation of "/tmp/tmpxft_000047d4_00000000-4_solve_csp.cpp4.ii".
    lib/CMakeFiles/cuda_csp.dir/build.make:75: recipe for target 'lib/CMakeFiles/cuda_csp.dir/solve_csp.cu.o' failed
    make[2]: *** [lib/CMakeFiles/cuda_csp.dir/solve_csp.cu.o] Error 2
    CMakeFiles/Makefile2:418: recipe for target 'lib/CMakeFiles/cuda_csp.dir/all' failed
    make[1]: *** [lib/CMakeFiles/cuda_csp.dir/all] Error 2
    Makefile:94: recipe for target 'all' failed
    make: *** [all] Error 2
    ```

---
+++ lib/solve_csp.cu
@@ -0,0 +1,71 @@
+#include <constraint_graph.hpp>
+#include <disparity_graph.hpp>
+#include <image.hpp>
+#include <indexing_checks.hpp>
+#include <indexing.hpp>
+#include <labeling_finder.hpp>
+#include <lowest_penalties.hpp>
+#include <solve_csp.hpp>
+#include <types.hpp>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#if !defined(__CUDA_ARCH__)
+using sp::types::ULONG;
+using sp::types::ULONG_ARRAY;
+using sp::types::Pixel;
+using sp::types::Edge;
+using sp::types::Node;
+using sp::image::Image;
+using sp::graph::disparity::DisparityGraph;
+using sp::graph::constraint::ConstraintGraph;
+using sp::graph::lowest_penalties::LowestPenalties;
+using sp::labeling::finder::choose_best_node;
+#endif
+
+__device__ void populate_structures_gpu(
+    struct DisparityGraph* disparity_graph,
+    struct LowestPenalties* lowest_penalties,
+    struct ConstraintGraph* constraint_graph,
+    int* nodes_availability,
+    unsigned* left_image,
+    unsigned* right_image,
+    float* min_penalties_pixels,
+    float* min_penalties_edges,
+    float* reparametrization,
+    unsigned height,
+    unsigned width,
+    unsigned max_value,
+    unsigned disparity_levels,
+    float threshold,
+    float cleanness,
+    float smoothness
+)
+{
+    #if defined(__CUDA_ARCH__)
+    disparity_graph->left.data = left_image;
+    disparity_graph->left.height = height;
+    disparity_graph->left.max_value = max_value;
+    disparity_graph->left.width = width;
+
+    disparity_graph->right.data = right_image;
+    disparity_graph->right.height = height;
+    disparity_graph->right.max_value = max_value;
+    disparity_graph->right.width = width;
+
+    disparity_graph->cleanness = cleanness;
+    disparity_graph->disparity_levels = disparity_levels;
+    disparity_graph->reparametrization = reparametrization;
+    disparity_graph->smoothness = smoothness;
+
+    lowest_penalties->graph = disparity_graph;
+    lowest_penalties->pixels = min_penalties_pixels;
+    lowest_penalties->neighborhoods = min_penalties_edges;
+
+    constraint_graph->threshold = threshold;
+    constraint_graph->nodes_availability = nodes_availability;
+    constraint_graph->lowest_penalties = lowest_penalties;
+    constraint_graph->disparity_graph = disparity_graph;
+    #endif
+}
