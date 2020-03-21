99e3ae3b5d596e16668d9b025e65618ac1881f7a
char-lie <5962274+char-lie@users.noreply.github.com>
2020-03-21 22:00:47 +0200

    Initialize CUDA calling code
    Add error checkers got from
    https://stackoverflow.com/a/14038590/1305036.

---
+++ lib/cuda_csp.cu
@@ -0,0 +1,23 @@
+#ifndef __CUDA_ARCH__
+#include <cuda_csp.hpp>
+#include <solve_csp.hpp>
+
+#include <cassert>
+
+#define cdpErrchk(ans) { cdpAssert((ans), __FILE__, __LINE__); }
+__host__ __device__ void cdpAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      printf("GPU kernel assert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) assert(0);
+   }
+}
+
+#define cudaCheckError() {                                          \
+ cudaError_t e=cudaGetLastError();                                 \
+ if(e!=cudaSuccess) {                                              \
+   printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));           \
+   exit(0); \
+ }                                                                 \
+}
