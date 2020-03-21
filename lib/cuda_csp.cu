2d59dd9ef9f5e9e1cf53d2b290b0c9f351e78839
char-lie <5962274+char-lie@users.noreply.github.com>
2020-03-21 22:44:37 +0200

    Add CUDA program variables initialization
    You should use `cudaMalloc` to allocate the GPU memory
    (just like `malloc`),
    and then use `cudaMemcpy` (like `memcpy`)
    to load contents of local arrays into the GPU memory.
    In the end, we should copy the result back
    and use `cudaFree` to free the memory (just like `free`).
    
    `cudaMemcpy` is different from `memcpy`
    in the flag that should be provided:
    `cudaMemcpyHostToDevice` if you want to copy from the host to GPU
    and `cudaMemcpyDeviceToHost` in the reverse case.
    
    Use the guide to know more
    https://devblogs.nvidia.com/how-optimize-data-transfers-cuda-cc/

--- lib/cuda_csp.cu
+++ lib/cuda_csp.cu
@@ -2,6 +2,7 @@
 #include <cuda_csp.hpp>
 #include <solve_csp.hpp>
 
+#include <vector>
 #include <cassert>
 
 #define cdpErrchk(ans) { cdpAssert((ans), __FILE__, __LINE__); }
@@ -21,3 +22,144 @@ __host__ __device__ void cdpAssert(cudaError_t code, const char *file, int line,
    exit(0); \
  }                                                                 \
 }
+
+namespace gpu
+{
+
+using std::vector;
+
+void prepare_problem(struct ConstraintGraph* graph, struct CUDAProblem* problem)
+{
+    vector<int> changed{0, 0};
+    vector<unsigned> left_image(
+        graph->disparity_graph->left.data.begin(),
+        graph->disparity_graph->left.data.end()
+    );
+    vector<float> min_penalties_edges(
+        graph->lowest_penalties->neighborhoods.begin(),
+        graph->lowest_penalties->neighborhoods.end()
+    );
+    vector<float> min_penalties_pixels(
+        graph->lowest_penalties->pixels.begin(),
+        graph->lowest_penalties->pixels.end()
+    );
+    vector<int> nodes_availability(
+        graph->nodes_availability.begin(),
+        graph->nodes_availability.end()
+    );
+    vector<float> reparametrization(
+        graph->disparity_graph->reparametrization.begin(),
+        graph->disparity_graph->reparametrization.end()
+    );
+    vector<unsigned> right_image(
+        graph->disparity_graph->right.data.begin(),
+        graph->disparity_graph->right.data.end()
+    );
+
+    cdpErrchk(cudaMalloc(
+        (void**)&(problem->changed),
+        2 * sizeof(int))
+    );
+    cdpErrchk(cudaMalloc(
+        (void**)&(problem->changed),
+        changed.size() * sizeof(changed[0]))
+    );
+    cdpErrchk(cudaMalloc(
+        &(problem->left_image),
+        left_image.size() * sizeof(left_image[0]))
+    );
+    cdpErrchk(cudaMalloc(
+        &(problem->min_penalties_edges),
+        min_penalties_edges.size() * sizeof(min_penalties_edges[0]))
+    );
+    cdpErrchk(cudaMalloc(
+        &(problem->min_penalties_pixels),
+        min_penalties_pixels.size() * sizeof(min_penalties_pixels[0]))
+    );
+    cdpErrchk(cudaMalloc(
+        &(problem->nodes_availability),
+        nodes_availability.size() * sizeof(nodes_availability[0]))
+    );
+    cdpErrchk(cudaMalloc(
+        &(problem->reparametrization),
+        reparametrization.size() * sizeof(reparametrization[0]))
+    );
+    cdpErrchk(cudaMalloc(
+        &(problem->right_image),
+        right_image.size() * sizeof(right_image[0]))
+    );
+
+    cdpErrchk(cudaMemcpy(
+        problem->changed,
+        changed.data(),
+        changed.size() * sizeof(changed[0]),
+        cudaMemcpyHostToDevice)
+    );
+    cdpErrchk(cudaMemcpy(
+        problem->left_image,
+        left_image.data(),
+        left_image.size() * sizeof(left_image[0]),
+        cudaMemcpyHostToDevice)
+    );
+    cdpErrchk(cudaMemcpy(
+        problem->min_penalties_edges,
+        min_penalties_edges.data(),
+        min_penalties_edges.size() * sizeof(min_penalties_edges[0]),
+        cudaMemcpyHostToDevice)
+    );
+    cdpErrchk(cudaMemcpy(
+        problem->min_penalties_pixels,
+        min_penalties_pixels.data(),
+        min_penalties_pixels.size() * sizeof(min_penalties_pixels[0]),
+        cudaMemcpyHostToDevice)
+    );
+    cdpErrchk(cudaMemcpy(
+        problem->nodes_availability,
+        nodes_availability.data(),
+        nodes_availability.size() * sizeof(nodes_availability[0]),
+        cudaMemcpyHostToDevice)
+    );
+    cdpErrchk(cudaMemcpy(
+        problem->reparametrization,
+        reparametrization.data(),
+        reparametrization.size() * sizeof(reparametrization[0]),
+        cudaMemcpyHostToDevice)
+    );
+    cdpErrchk(cudaMemcpy(
+        problem->right_image,
+        right_image.data(),
+        right_image.size() * sizeof(right_image[0]),
+        cudaMemcpyHostToDevice)
+    );
+}
+
+void free_problem(struct ConstraintGraph* graph, struct CUDAProblem* problem)
+{
+    vector<int> nodes_availability(graph->nodes_availability.size());
+    cdpErrchk(cudaMemcpy(
+        nodes_availability.data(),
+        problem->nodes_availability,
+        nodes_availability.size() * sizeof(nodes_availability[0]),
+        cudaMemcpyDeviceToHost)
+    );
+
+    graph->nodes_availability.assign(
+        nodes_availability.begin(),
+        nodes_availability.end()
+    );
+
+    cdpErrchk(cudaDeviceSynchronize());
+
+    cdpErrchk(cudaFree(problem->changed));
+    cdpErrchk(cudaFree(problem->left_image));
+    cdpErrchk(cudaFree(problem->min_penalties_edges));
+    cdpErrchk(cudaFree(problem->min_penalties_pixels));
+    cdpErrchk(cudaFree(problem->nodes_availability));
+    cdpErrchk(cudaFree(problem->reparametrization));
+    cdpErrchk(cudaFree(problem->right_image));
+
+    cdpErrchk(cudaDeviceSynchronize());
+}
+
+}
+#endif
