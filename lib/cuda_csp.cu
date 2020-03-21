43d40d5d10587ca023257a03182cdcd6efa9905d
char-lie <5962274+char-lie@users.noreply.github.com>
2020-03-21 23:26:33 +0200

    Abort the execution on CUDA exception

--- lib/cuda_csp.cu
+++ lib/cuda_csp.cu
@@ -7,12 +7,12 @@
 #include <vector>
 
 #define cdpErrchk(ans) { cdpAssert((ans), __FILE__, __LINE__); }
-__host__ __device__ void cdpAssert(cudaError_t code, const char *file, int line, bool abort=true)
+__host__ __device__ void cdpAssert(cudaError_t code, const char *file, int line)
 {
    if (code != cudaSuccess)
    {
       printf("GPU kernel assert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) assert(0);
+      assert(0);
    }
 }
 
